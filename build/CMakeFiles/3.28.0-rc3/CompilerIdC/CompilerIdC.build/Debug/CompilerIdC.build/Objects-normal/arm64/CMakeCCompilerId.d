2ee6cfd0e9b4a2a2e46be57991d22a2f4b841ab6
Jon Shiach <jon.shiach@gmail.com>
2025-03-10 08:44:05 +0000

    Added assets folder

--- build/CMakeFiles/3.28.0-rc3/CompilerIdC/CompilerIdC.build/Debug/CompilerIdC.build/Objects-normal/arm64/CMakeCCompilerId.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/jon/Library/CloudStorage/Dropbox/Work/2024_2025/Graphics/Coursework/Graphics_coursework/build/CMakeFiles/3.28.0-rc3/CompilerIdC/CMakeCCompilerId.c
--- build/build/Computer_Graphics_Coursework.build/Debug/Objects-normal/arm64/light.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/jon/Library/CloudStorage/Dropbox/Work/2024_2025/Graphics/Coursework/Graphics_coursework/common/light.cpp
