c3813d899a37327f53acedbb74ba9b14b046d7b4
Jon Shiach <jon.shiach@gmail.com>
2025-03-10 16:21:47 +0000

    Latest update

--- build/CMakeFiles/3.28.0-rc3/CompilerIdC/CompilerIdC.build/Debug/CompilerIdC.build/Objects-normal/arm64/CMakeCCompilerId.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/55093290/Library/CloudStorage/Dropbox/Work/2024_2025/Graphics/Coursework/Computer-Graphics-Coursework/build/CMakeFiles/3.28.0-rc3/CompilerIdC/CMakeCCompilerId.c
--- build/readme.txt
+++
@@ -1,1 +0,0 @@
-This is the build folder which you should use with CMake to build the project files.
